4e62b9afd1e11d7f5f762c10715449b89386dff1
Cecilia <cema3961@colorado.edu>
2018-06-29 16:33:16 -0600

    Forward method

--- code/MaoEtAl_full.py
+++ code/MaoEtAl_full.py
@@ -5,45 +5,29 @@ import torchvision.models as models
 
 torch.manual_seed(1)
 
+from MaoEtAl_baseline import LanguagePlusImage
 from TruncatedImageNetworks import TruncatedVGGorAlex
-from LSTM import LanguageModel
 from ClassifierHelper import Classifier
 from ReferExpressionDataset import ReferExpressionDataset
 
 from refer_python3.refer import REFER
 
 #Network Definition
-class LanguagePlusImage(Classifier):
-
-    def __init__(self, checkpt_file=None, vocab=None, hidden_dim=None, dropout=0, use_cuda=False):
-        super(LanguagePlusImage, self).__init__(use_cuda)
-
-        if checkpt_file is not None:
-            m = re.search('hidden(?P<hidden>\d+)_feats(?P<feats>\d+)_dropout(?P<dropout>\d+)', checkpt_file)
-            self.hidden_dim = int(m.group('hidden'))
-            self.feats_dim = int(m.group('feats'))
-            self.dropout_p = float(m.group('dropout'))
-        else:
-            self.feats_dim = 2005
-            self.hidden_dim = hidden_dim
-            self.dropout_p = dropout
-
-        #Text Embedding Network
-        self.wordnet = LanguageModel(vocab=vocab, additional_feat=self.feats_dim, hidden_dim=self.hidden_dim,
-                                     dropout=self.dropout_p, use_cuda=self.use_cuda)
-
-        #Image Embedding Network
-        self.imagenet = TruncatedVGGorAlex(models.vgg16(pretrained=True), maxpool=True, fix_weights=range(40))
-
-        self.to(self.device)
-        if checkpt_file is not None:
-            super(LanguagePlusImage, self).load_model(checkpt_file)
+class LanguagePlusImage_Contrast(LanguagePlusImage):
 
     def forward(self, ref, parameters):
-        ref['feats'] = self.image_forward(ref)
+        feats, contrast = self.image_forward(ref)
 
         #Input to LanguageModel
-        return self.wordnet(ref=ref)
+        ref['feats'] = feats
+        correct_embedding = self.wordnet(ref=ref)
+
+        contrast_embedding = []
+        for vector in contrast:
+            ref['feats'] = vector
+            contrast_embedding.append(self.wordnet(ref=ref))
+
+        return correct_embedding, contrast_embedding
 
     def image_forward(self, ref):
         # Global feature
@@ -62,27 +46,17 @@ class LanguagePlusImage(Classifier):
         # [top_left_x / W, top_left_y/H, bottom_left_x/W, bottom_left_y/H, size_bbox/size_image]
         pos = ref['pos']
 
+        # Contrast objects
+        contrast = ref['contrast']
+        contrast_out = []
+        for item in contrast:
+            contrast_item = item['object']
+            if self.use_cuda:
+                contrast_item = contrast_item.cuda()
+            contrast_out.append(torch.cat([image_out, self.imagenet(contrast_item), item['pos']], 1))
+
         # Concatenate image representations
-        return torch.cat([image_out, object_out, pos], 1)
-
-    def trim_batch(self, instance):
-        return self.wordnet.trim_batch(instance)
-
-    def clear_gradients(self, batch_size):
-        super(LanguagePlusImage, self).clear_gradients()
-        self.wordnet.clear_gradients(batch_size)
-
-    @staticmethod
-    def get_checkpt_file(checkpt_file, hidden_dim, feats_dim, dropout_p):
-        return '{}_hidden{}_feats{}_dropout{}.mdl'.format(checkpt_file, hidden_dim, feats_dim, dropout_p)
-
-    def checkpt_file(self, checkpt_prefix):
-        return self.get_checkpt_file(checkpt_prefix, self.hidden_dim, self.feats_dim, self.dropout_p)
-
-    def generate(self, start_word, ref):
-        self.eval()
-        feats = self.image_forward(ref)
-        return self.wordnet.generate(start_word, feats)
+        return torch.cat([image_out, object_out, pos], 1), contrast_out
 
 if __name__ == "__main__":
 
