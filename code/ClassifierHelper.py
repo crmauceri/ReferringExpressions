Fix off by one error

code/ClassifierHelper.py
@@ -66,17 +66,16 @@ class Classifier(nn.Module):
 
             for i_batch, sample_batched in enumerate(tqdm(dataloader, desc='{}rd epoch'.format(epoch))):
 
-                if i_batch > 2380:
-                    instances, targets = self.trim_batch(sample_batched)
-                    self.clear_gradients(batch_size)
+                instances, targets = self.trim_batch(sample_batched)
+                self.clear_gradients(batch_size)
 
-                    label_scores = self(instances, parameters)
-                    loss = self.loss_function(label_scores.permute(1, 2, 0), targets)
+                label_scores = self(instances, parameters)
+                loss = self.loss_function(label_scores.permute(1, 2, 0), targets)
 
-                    loss.backward()
-                    optimizer.step()
+                loss.backward()
+                optimizer.step()
 
-                    self.total_loss[epoch] += loss.item()
+                self.total_loss[epoch] += loss.item()
 
             self.total_loss[epoch] = self.total_loss[epoch] / float(i_batch)
 

code/LSTM.py
@@ -32,7 +32,7 @@ class LanguageModel(Classifier):
         self.word2idx = dict(zip(vocab, range(len(vocab))))
         self.ind2word = vocab
         self.vocab_dim = len(vocab)
-        self.embedding = torch.nn.Embedding(self.vocab_dim, self.embed_dim, padding_idx=0)
+        self.embedding = torch.nn.Embedding(self.vocab_dim+1, self.embed_dim, padding_idx=0)
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states with dimensionality hidden_dim
         self.dropout1 = nn.Dropout(p=self.dropout_p)