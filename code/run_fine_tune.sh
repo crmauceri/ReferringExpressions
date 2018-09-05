72cc0fb7017ac6ff5fdcbe10ff47803bb19a52c7
Cecilia <cema3961@colorado.edu>
2018-09-04 18:27:10 -0600

    Is batch norm a magic bullet?

--- code/run_fine_tune.sh
+++ code/run_fine_tune.sh
@@ -7,3 +7,5 @@ python code/MaoEtAl_baseline.py train models/maoetal_finetune.checkpoint10 --epo
 python code/MaoEtAl_baseline.py train models/maoetal_sunspot --epochs 10 --learningrate .001 --dataset sunspot --splitBy boulder --img_root pyutils/refer_python3/data/images/SUNRGBD/ --l2_fraction 0.0
 
 python code/MaoEtAl_baseline.py train models/maoetal_sunspot --epochs 10 --learningrate .001 --dataset sunspot --splitBy boulder --img_root pyutils/refer_python3/data/images/SUNRGBD/ --l2_fraction 1e-5
+
+python code/MaoEtAl_baseline.py train models/maoetal_finetune_batch --l2_fraction 1e-5 --batch_size 32 --learningrate 0.00001 --epochs 35 --dataset sunspot --splitBy boulder --img_root pyutils/refer_python3/data/images/SUNRGBD/
