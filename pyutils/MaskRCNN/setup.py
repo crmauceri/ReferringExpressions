f3f63d2fd66512b98e614e0c03de0b7b0bb26798
Cecilia <cema3961@colorado.edu>
2019-02-07 13:13:41 -0700

    Update submodules

--- pyutils/MaskRCNN/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name='MaskRCNN',
-      version='1.0',
-      packages=['MaskRCNN'],
-      )
--- pyutils/install.sh
+++ pyutils/install.sh
@@ -1,8 +1,14 @@
 pip install -e DepthAwareCNN
-pip install -e MaskRCNN
 pip install -e refer_python3/refer
 pip install -e refer_python3
+
+#Coco
 cd cocoapi/PythonAPI/
 make
 python setup.py install
 cd ../..
+
+#MaskRCNN
+cd maskrcnn-benchmark/
+python setup.py build develop
+cd ..
