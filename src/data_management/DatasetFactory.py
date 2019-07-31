fd7e6ce074f7af22fed621703dc7d103a709f2f4
Cecilia <cema3961@colorado.edu>
2019-07-30 21:45:38 -0600

    Add another dataset type for SUNRGBD

--- src/data_management/DatasetFactory.py
+++ src/data_management/DatasetFactory.py
@@ -6,7 +6,7 @@ def datasetFactory(cfg):
         from .refer import REFER
         refer = REFER(cfg)
         return ReferExpressionDataset(cfg, refer)
-    elif cfg.DATASET.CLASS == "ImageDataset":
+    elif cfg.DATASET.CLASS == "CocoDataset":
         from pycocotools.coco import COCO
         from .ReferExpressionDataset import ImageDataset
         import os
@@ -18,6 +18,20 @@ def datasetFactory(cfg):
         test = ImageDataset(cfg, test_data, img_root=cfg.DATASET.IMG_VAL_ROOT)
 
         val_data = COCO(os.path.join(cfg.DATASET.DATA_ROOT, 'instances_minival2014.json'))
+        val = ImageDataset(cfg, val_data, img_root=cfg.DATASET.IMG_VAL_ROOT)
+        return train, test, val
+    elif cfg.DATASET.CLASS == "ImageDataset":
+        from pycocotools.coco import COCO
+        from .ReferExpressionDataset import ImageDataset
+        import os
+
+        train_data = COCO(os.path.join(cfg.DATASET.DATA_ROOT, 'instances_train.json'))
+        train = ImageDataset(cfg, train_data)
+
+        test_data = COCO(os.path.join(cfg.DATASET.DATA_ROOT, 'instances_test.json'))
+        test = ImageDataset(cfg, test_data, img_root=cfg.DATASET.IMG_VAL_ROOT)
+
+        val_data = COCO(os.path.join(cfg.DATASET.DATA_ROOT, 'instances_val.json'))
         val = ImageDataset(cfg, val_data, img_root=cfg.DATASET.IMG_VAL_ROOT)
         return train, test, val
     else:
